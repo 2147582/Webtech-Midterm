f5c898b012e9e8bdd1759fb3a622829d0d7f68cb
2147582 <2147582@slu.edu.ph>
2018-10-22 19:26:43 +0800

    Calibrated page responsiveness

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -7,10 +7,16 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("about").innerHTML =
+      this.responseText;
+    }
+    if (this.readyState == 4 && this.status == 200) {
       document.getElementById("rescue1").innerHTML =
       this.responseText;
     }
   };
+  xhttp.open("GET", "about.html", true);
+  xhttp.send();
   xhttp.open("GET", "rescue-me.html", true);
   xhttp.send();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,28 @@
 */
 
 function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
+    var fname = document.forms["myForm"]["fname"].value;
+    var lname = document.forms["myForm"]["lname"].value;
+    var age = document.forms["myFrom"]["age"].value;
+    var gender = document.forms["myFrom"]["gender"].value;
+    
+    if (fname == "") {
+        alert("First name must be filled out.");
+        return false;
+    }
+    
+    if (lname == "") {
+        alert("Last name must be filled out.");
+        return false;
+    }
+    
+    if (age == "") {
+        alert("Indicate at least the age range.");
+        return false;
+    }
+    
+    if (gender == "") {
+        alert("Please indicate the gender of the person.");
         return false;
     }
 }
